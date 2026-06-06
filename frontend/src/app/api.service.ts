4604c70c2e73493e8f48218c10f008f1850d3a8f
safeetech <safeetech.paris@gmail.com>
2026-06-06 13:00:16 +0200

    prerender called handled

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,16 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Inject, Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { User } from './models/User.model';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, @Inject('platformId') private platformId: Object){}
 
   getUsers(): Observable<User[]>{
+    if (isPlatformBrowser(this.platformId)) {
+      return of([]); // Return an empty array if running in the browser
+    }
     return this.http.get<User[]>('/api/users');
   }
 }
